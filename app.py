641b391036fc582e37f54b1105331178558e9a95
Huy Gia Han (Karen) Vu <143664076+karengiahan@users.noreply.github.com>
2025-04-09 16:51:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ def log_email():
     password = request.form.get("password")
 
     if email or (username and password):
-        # with open("credentials.txt", "a") as file:
-        #     file.write(f"Email: {email if email else 'N/A'}, Username: {username}, Password: {password}\n")
         with open("email_log.txt", "a") as file:
             file.write(f"Email clicked: {email}"
                        f" - Username Entered: {'Yes' if username else 'No'} "
@@ -28,45 +26,3 @@ def server_down():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
-
-
-
-# from flask import Flask, render_template, request, redirect, url_for
-# app = Flask(__name__)
-# 
-# # @app.route('/log', methods=['POST'])
-# # def log_credentials():
-# #     username = request.form.get('username')
-# #     password = request.form.get('password')
-# #
-# #     with open("credentials.txt", "a") as f:
-# #         f.write(f"Username:{username}, Password:{password}\n")
-# #
-# #     return redirect(url_for('server_down'))
-# 
-# @app.route('/')
-# def login_page():
-#     return render_template('login_page.html')
-# 
-# @app.route('/log', methods=['GET'])
-# def log_email():
-#     email = request.args.get("email")
-#     username = request.args.get("username")
-#     password = request.args.get("password")
-# 
-#     if email:
-#         with open("email_log.txt", "a") as file:
-#             # file.write(f"Email clicked: {email}"
-#             #            f" - Username Entered: {'Yes' if username else 'No'} "
-#             #            f"- Password Entered: {'Yes' if password else 'No'}\n")
-#             file.write(f"Email clicked: {email}, Username entered: {username}, Password entered: {password}\n")
-# 
-#     return redirect(url_for('server_down'))
-# 
-# @app.route('/server-down')
-# def server_down():
-#     return render_template('khoury_server_down.html')
-# 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=8080, debug=True)
-# 
